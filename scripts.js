3328042ade9bd43b3567b26c03856808cc100c82
inafontan@gmail.com <you@example.com>
2022-03-14 12:26:23 -0300

    Incorporando Librerías

--- scripts.js
+++ scripts.js
@@ -34,6 +34,12 @@ let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
   
 btnCarrito.addEventListener("click", () => {
     sidebar.classList.toggle("active");
+    if(carrito.length === 0){
+      Swal.fire({
+        title: "Carrito Vacío",
+        icon: "warning",
+      });
+    }
 });  
   
 const cargarProductos = () => {               // CREACIÓN DE LOS PRODUCTOS 
@@ -41,13 +47,11 @@ const cargarProductos = () => {               // CREACIÓN DE LOS PRODUCTOS
     main.innerHTML += `
             <div class="tarjeta" >
               <img class="img-mercaderia" src="${element.imagen}">     
-                
               <div class="datos-mercaderia">
                  <p class="nombre">${element.nombre}</p>
                  <p class="precio"> $ <span>${element.precio}</span> </p>
-               <button class="btn-agregar" data-id="${element.id}">Agregar</button>
+                 <button class="btn-agregar" data-id="${element.id}">Agregar</button>
               </div>
-  
             </div>`;
 });
   const btnAgregar = document.querySelectorAll(".btn-agregar");
@@ -59,70 +63,86 @@ const cargarProductos = () => {               // CREACIÓN DE LOS PRODUCTOS
       })
     );
   };
-  
-  const agregarAlCarrito = (cardPadre) => {
-    let producto = {
-      nombre: cardPadre.querySelector(".nombre").textContent,
-      precio: Number(cardPadre.querySelector(".precio span").textContent),
-      cantidad: 1,
-      imagen: cardPadre.parentElement.querySelector("img").src,
-      id: Number(cardPadre.querySelector("button").getAttribute("data-id")),
-    };
-  
-    let productoEncontrado = carrito.find(
-      (element) => element.id === producto.id
-    );
-  
-    if (productoEncontrado) {
-    productoEncontrado.cantidad++;
-    } else {
-      carrito.push(producto);
-    }
+
+const swaltToast = (texto,color,position) => {
+  Swal.fire({                                     //Alerta producto agregado
+    text: texto,
+    background: color,
+    position: position,
+    icon: "success",
+    toast: true,
+    timer: 1500,
+    showConfirmButton: false,
+  });  
+}
+const agregarAlCarrito = (cardPadre) => {
+  swaltToast("Producto agregado", "#ffff00", "top-end");
     
-    console.log(carrito);
-    mostrarCarrito();
+  let producto = {
+    nombre: cardPadre.querySelector(".nombre").textContent,
+    precio: Number(cardPadre.querySelector(".precio span").textContent),
+    cantidad: 1,
+    imagen: cardPadre.parentElement.querySelector("img").src,
+    id: Number(cardPadre.querySelector("button").getAttribute("data-id")),
   };
   
-  const mostrarCarrito = () => {            // MUESTRA PRODUCTOS AGREGADOS
-    sidebar.innerHTML = "";
-    carrito.forEach((element) => {
-      let { imagen, nombre, precio, cantidad, id } = element;
-      sidebar.innerHTML += `
-          <div class="caja--carrito" >
-            <img class="caja-carrito-img" src="${imagen}">
+  let productoEncontrado = carrito.find(
+    (element) => element.id === producto.id
+  );
+  
+  if (productoEncontrado) {
+  productoEncontrado.cantidad++;
+  } else {
+    carrito.push(producto);
+  }
+    
+  console.log(carrito);
+  mostrarCarrito();
+};
+  
+const mostrarCarrito = () => {            // MUESTRA PRODUCTOS AGREGADOS
+  sidebar.innerHTML = "";
+  carrito.forEach((element) => {
+    let { imagen, nombre, precio, cantidad, id } = element;
+    sidebar.innerHTML += `
+        <div class="caja--carrito" >
+          <img class="caja-carrito-img" src="${imagen}">
+          <div class="caja--carrito--datos">
+            <p class="nombre">${nombre}</p>
+            <p class="cantidad">CANTIDAD: ${cantidad}</p>
+            <p class="subtotal">Subtotal: $${precio * cantidad}</p>
+            <p class="precio"> $ <span>${precio}</span> </p>
+          <button class="btn-restar" data-id="${id}">-</button>
+          <button class="btn-borrar" data-id="${id}">BORRAR</button>
             
-            <div class="caja--carrito--datos">
-              <p class="nombre">${nombre}</p>
-              <p class="cantidad">CANTIDAD: ${cantidad}</p>
-              <p class="subtotal">Subtotal: $${precio * cantidad}</p>
-              <p class="precio"> $ <span>${precio}</span> </p>
-            <button class="btn-restar" data-id="${id}">-</button>
-            <button class="btn-borrar" data-id="${id}">BORRAR</button>
-            
-            </div>
+          </div>
   
           </div>`;
     });
     localStorage.setItem("carrito", JSON.stringify(carrito));         // GUARDA PRODUCTOS AGREGADOS
     aumentarNumeroCantidadCarrito();
+  
   };
   
-  const restarProducto = (productoRestar) => {              // MODIFICAR ELEMENTOS AGREGADOS
+  const restarProducto = (productoRestar) => {                        // MODIFICAR ELEMENTOS AGREGADOS
+    
+    swaltToast("Producto Eliminado","#ffff00", "top-end");
+    
     let productoEncontrado = carrito.find(
       (element) => element.id === Number(productoRestar)
     );
     if (productoEncontrado) {
       productoEncontrado.cantidad--;
       if (productoEncontrado.cantidad === 0) {
-        productoEncontrado.cantidad = 1;
+        borrarProducto(productoRestar);
       }
     }
-    mostrarCarrito();
+    mostrarCarrito()
   };
   
   const borrarProducto = (productoBorrar) => {
     carrito = carrito.filter((element) => element.id !== Number(productoBorrar));
-    mostrarCarrito();
+    mostrarCarrito()
   };
   
   const escucharBotonesSidebar = () => {
@@ -140,36 +160,36 @@ const cargarProductos = () => {               // CREACIÓN DE LOS PRODUCTOS
     let total = carrito.reduce((acc, ite) => acc + ite.cantidad, 0);
     document.querySelector(".cant--carrito").textContent = total;
   };
-  
-const calcularTotal =()=>{
-  if(carrito.lenght !==0) {
+
+  const calcularTotal =()=> {
+  if(carrito.length !== 0) { 
   let total = carrito.reduce(
     (acc,ite) => acc + ite.precio * ite.cantidad,
     0
   );
-
+  
   let divTotal = document.createElement("div");
   divTotal.className = "caja";
   divTotal.Total.id = "total--compra";
 
-  divTotal.innerHTML = `<p> TOTAL $${total}</p><button>Finalizar compra</button>`;
+  divTotal.innerHTML = `<p>TOTAL $${total}</p><button>Finalizar compra</button>`;
   sidebar.appendChild(divTotal);
    
-  let botonFinalizar = document.querySelector("#total--compra");
+  let botonFinalizar = document.querySelector("#total--compra")
   botonFinalizar.onclick = () => {
     const mixin = Swal.mixin();
 
     mixin.fire({
       title: "Complete sus datos:",
-      html: `Nro Tarjeta <input id="tarjeta" type="number" class="swal12"> <br>
-             Domicilio <input id="domicilio" type="text">
+      html: `<input id="tarjeta" type="number" class="swal2-input" placeholder= "Nro Tarjeta"> <br>
+             <input id="domicilio" type="text" class="swal2-input" placeholder="Domicilio">
+             <p>Total : $${total}</p>
              `,
       confirmButtonText:"Comprar",
       showCancelButton: true,
       cancelButtonText: "Cancelar",
       showCloseButton: false,
       allowOutsideClick: false,
-
       preConfirm: ()=>{
         let domicilio = Swal.getPopup().querySelector("#domicilio").value
         if(!domicilio){
@@ -177,60 +197,22 @@ const calcularTotal =()=>{
         }
         return domicilio;
       },
-    }).then((response) =>{
-      console.log(response);
+    })
+    .then((response) =>{
       if(response.isConfirmed){
+        console.log(response);
         mixin.fire(
           "Compra realizada",
           "El pedido será enviado a "+ response.value, 
           "success"
-          );
-        }
+        );
+      }
+      carrito = [];
     });
   };
 }
 }
 
-  cargarProductos();
-  mostrarCarrito();
-  escucharBotonesSidebar();
-
-const producto = {nombre: "Miel XS", precio: 250, imagen: "./imagenes/XS.jfif", id: 0} 
-let { nombre, precio, imagen, id} = producto
-
-console.log (nombre)
-console.log (precio)
-
-const producto1 = {
-  nombre: "Miel XS", 
-  precio: 250, 
-  imagen: "./imagenes/XS.jfif", 
-  id: 0
-}
-
-const producto2 = {
-  producto1
-}
-
-console.log (producto2)
-
-const producto3 = {
-  producto1,
-  precio: 350, 
-  imagen: "./imagenes/L.jfif", 
-  id: 2
-}
-
-console.log (producto3)
-
-// DECLARACION Y ASIGANCION CONDICIONAL
-const compra = (producto.precio >= 250) ? true : false
-
-// MENSAJE
-compra ? console.log("Tiene descuento") : console.log("No tiene descuento")
-
-// OPERADOR AND
-const carroCompra = []
-
-carroCompra.length === 0 && console.log("No seleccionó ningún producto aún!")
-
+cargarProductos();
+mostrarCarrito();
+escucharBotonesSidebar();
